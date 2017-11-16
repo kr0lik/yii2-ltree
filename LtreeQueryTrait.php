71d554e3839d1754fcdff847cae98893e2751927
kr0lik <krolik@yandex.ru>
2017-11-16 13:36:33 +0300

    Update LtreeQueryTrait.php

--- LtreeQueryTrait.php
+++ LtreeQueryTrait.php
@@ -9,57 +9,57 @@ trait LtreeQueryTrait
 
     public function sorted($sort = SORT_ASC)
     {
-        $tb = ($this->modelClass)::tableName();
+        $tb = $this->getPrimaryTableName();
         return $this->orderBy(["$tb.path" => $sort]);
     }
 
     public function notRoot()
     {
-        $tb = ($this->modelClass)::tableName();
+        $tb = $this->getPrimaryTableName();
         return $this->andWhere(['>', "nlevel($tb.path)", 1]);
     }
 
     public function root()
     {
-        $tb = ($this->modelClass)::tableName();
+        $tb = $this->getPrimaryTableName();
         return $this->andWhere(["nlevel($tb.path)" => 1]);
     }
 
     public function byPath($path, $recursive = true)
     {
-        $tb = ($this->modelClass)::tableName();
+        $tb = $this->getPrimaryTableName();
         return $this->andWhere([$recursive ? '<@' : '=', "$tb.path", $path]);
     }
 
     public function joinParents(int $level = 0, string $joinType = 'LEFT JOIN')
     {
         return $this->joinWith(['parents' => function ($query) use ($level) {
-            $table = ($this->modelClass)::tableName();
+            $tb = $this->getPrimaryTableName();
 
-            $query->from(['parents' => $table])
-                ->onCondition(['@>', new Expression("\"parents\".{$this->pathName}"), new Expression("$table.{$this->pathName}")])
-                ->andOnCondition(['<>', new Expression("\"parents\".{$this->pathName}"), new Expression("$table.{$this->pathName}")])
+            $query->from(['parents' => $tb])
+                ->onCondition(['@>', new Expression("\"parents\".{$this->pathName}"), new Expression("$tb.{$this->pathName}")])
+                ->andOnCondition(['<>', new Expression("\"parents\".{$this->pathName}"), new Expression("$tb.{$this->pathName}")])
                 ->where(false);
 
             $query->link = [];
 
-            if ($level) $query->andOnCondition(['>=', new Expression("nlevel(\"parents\".{$this->pathName})"),  new Expression("nlevel($table.{$this->pathName}) - :level")], ['level' => $level]);
+            if ($level) $query->andOnCondition(['>=', new Expression("nlevel(\"parents\".{$this->pathName})"),  new Expression("nlevel($tb.{$this->pathName}) - :level")], ['level' => $level]);
         }], false, $joinType);
     }
 
     public function joinChildrens(int $level = 0, string $joinType = 'LEFT JOIN')
     {
         return $this->joinWith(['childrens' => function ($query) use ($level) {
-            $table = ($this->modelClass)::tableName();
+            $tb = $this->getPrimaryTableName();
 
-            $query->from(['childrens' => $table])
-                ->onCondition(['<@', new Expression("\"childrens\".{$this->pathName}"), new Expression("$table.{$this->pathName}")])
-                ->andOnCondition(['<>', new Expression("\"childrens\".{$this->pathName}"), new Expression("$table.{$this->pathName}")])
+            $query->from(['childrens' => $tb])
+                ->onCondition(['<@', new Expression("\"childrens\".{$this->pathName}"), new Expression("$tb.{$this->pathName}")])
+                ->andOnCondition(['<>', new Expression("\"childrens\".{$this->pathName}"), new Expression("$tb.{$this->pathName}")])
                 ->where(false);
 
             $query->link = [];
 
-            if ($level) $query->andOnCondition(['>=', new Expression("nlevel(\"childrens\".{$this->pathName})"),  new Expression("nlevel($table.{$this->pathName}) + :level")], ['level' => $level]);
+            if ($level) $query->andOnCondition(['>=', new Expression("nlevel(\"childrens\".{$this->pathName})"),  new Expression("nlevel($tb.{$this->pathName}) + :level")], ['level' => $level]);
         }], false, $joinType);
     }
 }
